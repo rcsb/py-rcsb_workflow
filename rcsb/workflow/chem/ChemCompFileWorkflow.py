aa9c43fb7c4aeed70117e5bb7c518380ae20b193
jwest <john.westbrook@rcsb.org>
2020-05-16 12:20:32 -0400

    V0.16 Add ChemCompFileWorkflow()

---
+++ rcsb/workflow/chem/ChemCompFileWorkflow.py
@@ -0,0 +1,89 @@
+##
+# File: ChemCompFileWorkflow.py
+# Date: 10-Mar-2020  jdw
+#
+#  Workflow wrapper  --  chemical component file conversion generator --
+#
+#  Updates:
+#
+##
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Apache 2.0"
+
+import logging
+import os
+
+from rcsb.utils.chem.OeIoUtils import OeIoUtils
+from rcsb.utils.chem.OeMoleculeProvider import OeMoleculeProvider
+
+logger = logging.getLogger(__name__)
+
+
+class ChemCompFileWorkflow(object):
+    def __init__(self, **kwargs):
+        """ Module entry point for chemical component definition 2D image generation
+
+        Args:
+            ccUrlTarget (str): URL or path for concatenated chemical component dictionary (default: public wwPDB ftp)
+            birdUrlTarget (str): URL or path for concatenated BIRD dictionary (default: public wwPDB ftp)
+            licenseFilePath (str) = path to OpenEye license text file
+            fileDirPath(str): directory containing generated image tree
+            cachePath(str): cache directory for temporary files
+
+        """
+        #
+        ccUrlTarget = kwargs.get("ccUrlTarget", None)
+        birdUrlTarget = kwargs.get("birdUrlTarget", None)
+        self.__licensePath = kwargs.get("licenseFilePath", "oe_license.txt")
+        self.__fileDirPath = kwargs.get("fileDirPath", ".")
+        cachePath = kwargs.get("cachePath", ".")
+        cachePath = os.path.abspath(cachePath)
+
+        #
+        self.__oemp = OeMoleculeProvider(
+            ccUrlTarget=ccUrlTarget,
+            birdUrlTarget=birdUrlTarget,
+            ccFileNamePrefix="cc-full",
+            cachePath=cachePath,
+            molBuildType="ideal-xyz",
+            useCache=True,
+            oeFileNamePrefix="oe-full",
+        )
+        self.__oeMolD = self.__oemp.getOeMolD()
+
+    def __setLicense(self, licensePath):
+        ok = False
+        try:
+            if os.environ.get("OE_LICENSE") and os.access(os.environ["OE_LICENSE"], os.R_OK):
+                logger.info("Using license from environment %r", os.environ["OE_LICENSE"])
+                ok = True
+            elif os.access(licensePath, os.R_OK):
+                os.environ["OE_LICENSE"] = licensePath
+                logger.info("Setting environmenal variable OE_LICENSE to %r", os.environ["OE_LICENSE"])
+                ok = True
+        except Exception as e:
+            logger.error("Setting license file %r failing %s", licensePath, str(e))
+        return ok
+
+    def testCache(self):
+        return self.__oemp.testCache() if self.__oemp else False
+
+    def makeFiles(self, fmt="sdf"):
+        """ Create files (mol, mol2) for all public chemical components.
+        """
+        try:
+            if fmt not in ["mol", "mol2", "mol2h", "sdf"]:
+                return False
+            if not self.__setLicense(self.__licensePath):
+                logger.error("Invalid license details - exiting")
+                return False
+            for ccId, oeMol in self.__oeMolD.items():
+                filePath = os.path.join(self.__fileDirPath, fmt, ccId[0], ccId + "." + fmt)
+                oeioU = OeIoUtils()
+                oeioU.write(filePath, oeMol, constantMol=True)
+            return True
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+        return False
---
+++ rcsb/workflow/tests/testChemCompFileWorkflow.py
@@ -0,0 +1,77 @@
+##
+#
+# File:    ChemCompImageWorkflowTests.py
+# Author:  jdw
+# Date:    10-Mar-2020
+# Version: 0.001
+#
+# Updates:
+##
+"""
+A collection of tests chemical component file generation workflows
+
+"""
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Creative Commons Attribution 3.0 Unported"
+__version__ = "V0.01"
+
+import logging
+import os
+import platform
+import resource
+import time
+import unittest
+
+from rcsb.workflow.chem.ChemCompFileWorkflow import ChemCompFileWorkflow
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
+logger = logging.getLogger()
+
+
+class ChemCompImageWorkflowTests(unittest.TestCase):
+    def setUp(self):
+        self.__startTime = time.time()
+        self.__workPath = os.path.join(HERE, "test-output")
+        self.__cachePath = os.path.join(HERE, "test-output")
+        logger.debug("Running tests on version %s", __version__)
+        logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
+
+    def tearDown(self):
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
+        rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
+        endTime = time.time()
+        logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
+
+    def testMakeFiles(self):
+        """Test case -  generate all sdf and mol2 files for chemical component definitions.
+        """
+        try:
+            # Example uses default urls for public chemical dictionaries
+            ccfWf = ChemCompFileWorkflow(fileDirPath=self.__workPath, cachePath=self.__cachePath)
+            ok = ccfWf.testCache()
+            if not ok:
+                logger.error("File generation dependency generation failure")
+            self.assertTrue(ok)
+            #
+            ok = ccfWf.makeFiles(fmt="sdf")
+            self.assertTrue(ok)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+            self.fail()
+
+
+def suiteFileGeneration():
+    suiteSelect = unittest.TestSuite()
+    suiteSelect.addTest(ChemCompImageWorkflowTests("testMakeFiles"))
+    return suiteSelect
+
+
+if __name__ == "__main__":
+    mySuite = suiteFileGeneration()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
