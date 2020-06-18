d9ee0e712c8388691a7c37dc092529784f6d5621
jwest <john.westbrook@rcsb.org>
2020-06-18 19:18:34 -0400

    V0.17 Add ChemCompSearchIndexWorkflow()

---
+++ rcsb/workflow/chem/ChemCompSearchIndexWorkflow.py
@@ -0,0 +1,99 @@
+##
+# File: ChemCompIndexWorkflow.py
+# Date: 2-Jun-2020  jdw
+#
+#  Workflow wrapper  --  generate chemical component and BIRD search indices  --
+#
+#  Updates:
+#  10-Jun-2020 jdw Hookup to ChemCompSearchWrapper()
+##
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Apache 2.0"
+
+import logging
+import os
+
+from rcsb.utils.chem.ChemCompSearchWrapper import ChemCompSearchWrapper
+
+logger = logging.getLogger(__name__)
+
+
+class ChemCompSearchIndexWorkflow(object):
+    def __init__(self, **kwargs):
+        """ Module entry point for chemical component and BIRD search index generation workflow.
+
+            Args:
+                cachePath(str, optional): cache directory to store indices and temporary files (default: '.')
+                licenseFilePath (str, optional) = path to OpenEye license text file (default: use enviroment OE_LICENSE setting)
+                ccFilNamePrefix (str, optional) =  index prefix (default: "cc-full")
+        """
+        self.__licensePath = kwargs.get("licenseFilePath", "oe_license.txt")
+        cachePath = kwargs.get("cachePath", ".")
+        self.__cachePath = os.path.abspath(cachePath)
+        self.__ccFileNamePrefix = kwargs.get("ccFilNamePrefix", "cc-full")
+        self.__ccsw = ChemCompSearchWrapper(cachePath=self.__cachePath, ccFileNamePrefix=self.__ccFileNamePrefix)
+
+    def __setLicense(self, licensePath):
+        ok = False
+        try:
+            if os.environ.get("OE_LICENSE") and os.access(os.environ["OE_LICENSE"], os.R_OK):
+                logger.info("Using license from environment %r", os.environ["OE_LICENSE"])
+                ok = True
+            elif os.access(licensePath, os.R_OK):
+                os.environ["OE_LICENSE"] = licensePath
+                logger.info("Setting environmenal variable OE_LICENSE to %r", os.environ["OE_LICENSE"])
+                ok = True
+        except Exception as e:
+            logger.error("Setting Openeye license file %r failing %s", licensePath, str(e))
+        return ok
+
+    def testCache(self):
+        return True
+
+    def makeIndices(self, ccUrlTarget, birdUrlTarget, **kwargs):
+        """Build chemical component and BIRD search indices.
+
+            Args:
+                ccUrlTarget (str): URL or path for concatenated chemical component dictionary
+                birdUrlTarget (str): URL or path for concatenated BIRD dictionary
+
+                Other arguments may be supplied to change defaults for index generators (testing/troubleshooting)
+
+            Returns:
+                bool:  True for success or False otherwise
+
+        """
+        ok = False
+        if not self.__setLicense(self.__licensePath):
+            logger.error("Invalid OpenEye license details - exiting")
+            return ok
+        #
+        try:
+            ok = self.__ccsw.buildDependenices(ccUrlTarget, birdUrlTarget, **kwargs)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+        return ok
+
+    def stashIndices(self, url, dirPath, bundleLabel="A", userName=None, pw=None):
+        """ Store a copy of the bundled search dependencies remotely -
+
+        Args:
+            url (str): URL string for the destination host (e.g. sftp://myserver.net or None for a local file)
+            dirPath (str): directory path on the remote resource
+            bundleLabel (str, optional): optional label preppended to the stashed dependency bundle artifact (default='A')
+            userName (str, optional): optional access information. Defaults to None.
+            password (str, optional): optional access information. Defaults to None.
+
+        Returns:
+          bool:  True for success or False otherwise
+
+        """
+        #
+        ok = False
+        try:
+            ok = self.__ccsw.stashDependencies(url, dirPath, bundleLabel=bundleLabel, userName=userName, pw=pw)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+        return ok
---
+++ rcsb/workflow/tests/testChemCompSearchIndexWorkflow.py
@@ -0,0 +1,82 @@
+##
+#
+# File:    ChemCompImageWorkflowTests.py
+# Author:  jdw
+# Date:    10-Mar-2020
+# Version: 0.001
+#
+# Updates:
+##
+"""
+A collection of tests chemical component index generation workflows
+
+"""
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Creative Commons Attribution 3.0 Unported"
+__version__ = "V0.01"
+
+import logging
+import os
+import platform
+import resource
+import time
+import unittest
+
+from rcsb.workflow.chem.ChemCompSearchIndexWorkflow import ChemCompSearchIndexWorkflow
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
+logger = logging.getLogger()
+
+
+class ChemCompSearchIndexWorkflowTests(unittest.TestCase):
+    def setUp(self):
+        self.__startTime = time.time()
+        self.__copyPath = os.path.join(HERE, "test-output", "COPY")
+        self.__cachePath = os.path.join(HERE, "test-output", "CACHE")
+        self.__dataPath = os.path.join(HERE, "test-data")
+        abbrevTest = True
+        self.__ccUrlTarget = os.path.join(self.__dataPath, "components-abbrev.cif") if abbrevTest else None
+        self.__birdUrlTarget = os.path.join(self.__dataPath, "prdcc-abbrev.cif") if abbrevTest else None
+        self.__ccFileNamePrefix = "cc-abbrev" if abbrevTest else "cc-full"
+        logger.debug("Running tests on version %s", __version__)
+        logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
+
+    def tearDown(self):
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
+        rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
+        endTime = time.time()
+        logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
+
+    def testMakeAndStashIndices(self):
+        """Test case -  generate chemical component and BIRD search indices
+
+        Maximum resident memory size 7126 MB   6 procs on macbook pro (1374 seconds)
+        Maximum resident memory size 7479 MB   4 procs on macbook pro (1528 seconds)
+
+        """
+        try:
+            ccidxWf = ChemCompSearchIndexWorkflow(cachePath=self.__cachePath, ccFileNamePrefix=self.__ccFileNamePrefix)
+            ok = ccidxWf.makeIndices(self.__ccUrlTarget, self.__birdUrlTarget, numProc=4)
+            self.assertTrue(ok)
+            ok = ccidxWf.stashIndices(None, self.__copyPath, bundleLabel="A", userName=None, pw=None)
+            self.assertTrue(ok)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+            self.fail()
+
+
+def suiteIndexGeneration():
+    suiteSelect = unittest.TestSuite()
+    suiteSelect.addTest(ChemCompSearchIndexWorkflowTests("testMakeAndStashIndices"))
+    return suiteSelect
+
+
+if __name__ == "__main__":
+    mySuite = suiteIndexGeneration()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
