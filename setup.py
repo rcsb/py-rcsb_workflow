d2c5d45ef84e28b2259bc007d82ed2303ab95668
jwest <john.westbrook@rcsb.org>
2020-12-11 16:17:32 -0500

    Update dependencies

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     # entry_points={"console_scripts": ["cactvs_annotate_mol=rcsb.workflow.cactvsAnnotateMol:main"]},
     #  The following is somewhat flakey --
     # dependency_links=[],
-    install_requires=["rcsb.utils.chem >= 0.45", "rcsb.utils.seq >= 0.50", "rcsb.utils.targets >= 0.18"],
+    install_requires=["rcsb.utils.chem >= 0.45", "rcsb.utils.seq >= 0.50", "rcsb.utils.targets >= 0.18", "rcsb.exdb >= 0.68"],
     packages=find_packages(exclude=["rcsb.mock-data", "rcsb.workflow.tests", "rcsb.workflow.tests-*", "tests.*"]),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:
