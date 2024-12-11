8bc0c01e0bef09bb93badee8ba8279c1fd6c1beb
Michael Trumbull <michael.trumbull@rcsb.org>
2024-12-11 15:30:04 -0500

    added cli entry point for imgs functions

---
+++ rcsb/workflow/cli/imgs_cli.py
@@ -0,0 +1,55 @@
+"""Cli parser for imgs workflow."""
+
+import argparse
+from imgs_wf import images_gen_jpgs, images_gen_lists
+
+
+def main() -> None:
+    """Cli parser for images workflow."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--op",
+        required=True,
+        help="which function to call",
+        choices=[
+            "gen_lists",
+            "gen_jpgs",
+        ],
+    )
+
+    parser.add_argument("--pdb_gz_path")
+    parser.add_argument("--csm_gz_path")
+    parser.add_argument("--pdb_base_dir")
+    parser.add_argument("--csm_base_dir")
+    parser.add_argument("--update_all_images", action='store_true')
+    parser.add_argument("--imgs_exclude_models", action='store_true')
+    parser.add_argument("--num_workers")
+
+    parser.add_argument("--id_list_path")
+    parser.add_argument("--update_tmp_base")
+    parser.add_argument("--prerelease_ftp_file_base_path")
+    parser.add_argument("--csm_file_repo_base_path")
+    parser.add_argument("--bcif_exe")
+    parser.add_argument("--images_tmp_base")
+    parser.add_argument("--molrender_exe")
+    parser.add_argument("--jpgs_out_dir")
+
+    parser.add_argument("--file_number")
+
+    args = vars(parser.parse_args())
+
+    if args["op"] == "gen_lists":
+        images_gen_lists(args)
+    elif args["op"] == "gen_jpgs":
+        images_gen_jpgs(args)
+    else:
+        msg = "Cli --op flag error: not availible option"
+        raise ValueError(msg)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        msg = f"Run failed {err}"
+        raise RuntimeError(msg) from err
