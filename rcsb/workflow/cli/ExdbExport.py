5ba1470f474e8203cf038c2bb68619e9db7778aa
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2025-03-03 11:02:47 -0800

    refactor: separate Mongo export logic; sketch out incremental logic

--- rcsb/workflow/cli/ExdbExport.py
+++ rcsb/workflow/cli/ExdbExport.py
@@ -4,79 +4,19 @@ Command-line entry point for exporting or processing data from MongoDB.
 
 import logging
 import os
+import re
 import sys
-import shutil
-from argparse import ArgumentParser, Namespace
-from collections.abc import Generator, Sequence, Callable
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
+from collections.abc import Sequence, Mapping
 from dataclasses import dataclass
+from datetime import datetime
+from functools import partial
 from pathlib import Path
-from typing import ClassVar, Optional
+from typing import ClassVar, Optional, Union
 
-from bson.json_util import dumps as bson_dumps, RELAXED_JSON_OPTIONS, JSONOptions
-from pymongo import MongoClient
+from rcsb.workflow.mongo.IncrementalExporter import IncrementalExporter, IncrementalSource
 
 logger = logging.getLogger("rcsb-workflow")
-
-
-@dataclass(frozen=True, slots=True)
-class Source:
-    """How to connect to MongoDB and what data to read for export."""
-
-    client_factory: ClassVar[Callable[[str], MongoClient]] = MongoClient
-    db_uri: str
-    db_name: str
-    collection: str
-    fields: set[str]
-    skip: int = 0
-    limit: int = 0
-
-    def __call__(self) -> Generator[dict]:
-        projection: Optional[dict[str, bool]] = None
-        if self.fields:
-            # PyMongo includes `_id` if `projection` is a list or doesn't contain a key `_id`.
-            # To exclude `_id`, we need to specify `{"_id": False}`.
-            projection = {field: True for field in self.fields}
-            if "_id" not in self.fields:
-                projection |= {"_id": False}
-        with self.client_factory(self.db_uri) as client:
-            db = client[self.db_name][self.collection]
-            yield from db.find({}, projection=projection, skip=self.skip, limit=self.limit)
-
-
-@dataclass(frozen=True, slots=True)
-class Exporter:
-    """App that reads a MongoDB ``source`` and writes to a file."""
-
-    source: Source
-    json_options: JSONOptions = RELAXED_JSON_OPTIONS
-
-    def export_to(self, out_file: Path) -> None:
-        temp_file = self._get_temp_file(out_file)
-        try:
-            n_docs = self._export(temp_file)
-            shutil.move(temp_file, out_file)
-        except BaseException:
-            temp_file.unlink(missing_ok=True)
-            raise
-        logger.info(f"Wrote {n_docs} documents to {out_file}.")
-
-    def _export(self, temp_file: Path) -> int:
-        logger.info(f"Extracting documents from {self.source.collection}...")
-        n_docs = 0
-        with temp_file.open("w", encoding="utf-8", newline="\n") as f:
-            f.write("[\n")
-            for doc in self.source():
-                f.write(bson_dumps(doc, json_options=self.json_options))
-                if n_docs > 0:
-                    f.write(",\n")
-                n_docs += 1
-                if n_docs % 1000 == 0:
-                    logger.debug(f"Extracted {n_docs} documents.")
-            f.write("\n]\n")
-        return n_docs
-
-    def _get_temp_file(self, out_file: Path) -> Path:
-        return out_file.parent / f".{out_file.name}.temp"
 
 
 @dataclass(frozen=True, slots=True)
@@ -101,6 +41,43 @@ class LogConfig:
         logger.setLevel(max(self.default_level + level_offset, 0))
 
 
+class _Args:
+
+    @staticmethod
+    def int(s: str) -> int:
+        return min(int(s), 0)
+
+    @staticmethod
+    def utc_dt(s: str) -> datetime:
+        dt = datetime.fromisoformat(s)
+        if not s.endswith("Z") or dt.tzname() not in {"UTC", "Etc/UTC"}:
+            msg = f"date-time '{s}' is not UTC or does not end in 'Z'"
+            raise ArgumentTypeError(msg)
+        return dt
+
+    @staticmethod
+    def csv(s: str) -> Optional[set[str]]:
+        if s.strip() == "*":
+            return set()
+        return set(map(str.strip, s.split(",")))
+
+    @staticmethod
+    def out_file(s: str, kwargs: Mapping[str, Union[str, int, Path]]) -> Path:
+        pattern: re.Pattern = re.compile(r"\{([^:}]*)(:[^}]*)?}")
+        s = pattern.sub(partial(_Args._sub, kwargs=kwargs), s)
+        return Path(s)
+
+    @staticmethod
+    def _sub(m: re.Match, kwargs: Mapping[str, Union[str, int, Path]]) -> str:
+        var = m.group(1)
+        fb: Optional[str] = str(m.group(2).removeprefix(":")) if m.group(2) else None
+        if not var or fb and var not in kwargs or fb == "":
+            msg = f"Invalid substitution: '{m.group(0)}'"
+            raise ArgumentTypeError(msg)
+        value = kwargs.get(var, fb)
+        return value.name if isinstance(value, Path) else str(value)
+
+
 @dataclass(frozen=True, slots=True)
 class Main:
     """CLI entry point for reading MongoDB data."""
@@ -116,8 +93,8 @@ class Main:
         db_name = os.environ.get("MONGODB_NAME", self._DEFAULT_DB_NAME)
         match ns.subcommand:
             case "export":
-                source = Source(db_uri, db_name, ns.collection, ns.fields)
-                Exporter(source).export_to(ns.to)
+                source = IncrementalSource(db_uri, db_name, ns.collection, ns.fields | ns.id_fields, ns.since_field)
+                IncrementalExporter(source).export_to(ns.to)
 
     def _parser(self) -> ArgumentParser:
         sup = ArgumentParser(
@@ -129,27 +106,54 @@ class Main:
                 f"    MONGODB_NAME   The database name (default: {self._DEFAULT_DB_NAME})."
             ),
         )
-        sup.add_argument("-v", "--verbose", action="count", help="Decrement the log level (repeatable).")
-        sup.add_argument("-q", "--quiet", action="count", help="Increment the log level (repeatable).")
-        subs = sup.add_subparsers(title="subcommands", dest="subcommand", description="subcommands", required=True)
+        sup.add_argument(
+            "-v", "--verbose", action="count",
+            help="Decrement the log level (repeatable)."
+        )
+        sup.add_argument(
+            "-q", "--quiet", action="count",
+            help="Increment the log level (repeatable)."
+        )
+        subs = sup.add_subparsers(
+            title="subcommands", dest="subcommand", description="subcommands", required=True
+        )
         # Subcommand 1: `export`
-        export = subs.add_parser("export", allow_abbrev=False, help="Export a MongoDB collection to a JSON file.")
-        export.add_argument("collection", metavar="COLLECTION", help="Name of the MongoDB collection.")
-        export.add_argument("--fields", type=Main._csv, default="*", metavar="CSV-LIST", help="Comma-separated list of fields to export.")
-        export.add_argument("--to", type=Path, metavar="JSON-FILE", help="Output JSON file path (overwritten if it exists).")
-        export.add_argument("--skip", type=Main._int, default=0, metavar="COUNT", help="Number of documents to skip.")
-        export.add_argument("--limit", type=Main._int, default=0, metavar="COUNT", help="Max number of documents to read.")
+        export = subs.add_parser(
+            "export", allow_abbrev=False,
+            help=
+            "Compute a delta from a previous export to a current MongoDB collection."
+            "Documents to be removed will contain only the id fields."
+        )
+        export.add_argument(
+            "collection", metavar="COLLECTION",
+            help="Name of the MongoDB collection."
+        )
+        export.add_argument(
+            "--fields", type=_Args.csv, default="*", metavar="CSV",
+            help="List of fields to export."
+        )
+        export.add_argument(
+            "--id-fields", type=_Args.csv, default="_id", required=True, metavar="CSV",
+            help="List of fields needed to identify documents. Included in to-delete documents."
+        )
+        export.add_argument(
+            "--to", default="{collection}-{since:all}.json", metavar="JSON-FILE",
+            help="Output JSON file path. May refer to {collection}, {delta[:if-empty]}, and {since[:if-empty]}."
+        )
+        export.add_argument(
+            "--delta", type=Path, metavar="JSON-FILE",
+            help="Compute a delta from this previous export."
+        )
+        export.add_argument(
+            "--since", type=_Args.utc_dt, metavar="RFC-3339",
+            help="Only export docs where '--since-field' ≥ this UTC date-time. Must be RFC 3339 with a 'Z' offset."
+        )
+        export.add_argument(
+            "--since-field", default="timestamp", metavar="STR",
+            help="Name of the timestamp field for comparison with '--since'."
+        )
+        # Done
         return sup
-
-    @staticmethod
-    def _int(s: str) -> int:
-        return min(int(s), 0)
-
-    @staticmethod
-    def _csv(s: str) -> Optional[set[str]]:
-        if s.strip() == "*":
-            return set()
-        return set(map(str.strip, s.split(",")))
 
 
 def main() -> None:
