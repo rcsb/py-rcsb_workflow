7b66fcfd1d71226218261ea074ce80ac1b40c96a
Michael Trumbull <michael.trumbull@rcsb.org>
2025-04-07 11:22:55 -0400

    moved run_command into class

--- rcsb/workflow/wuw/PdbCsmImageWorkflow.py
+++ rcsb/workflow/wuw/PdbCsmImageWorkflow.py
@@ -26,26 +26,6 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(mo
 logger = logging.getLogger()
 
 
-# For pickling reasons this command needs to not accept self
-def run_command(args):
-    """Run a command and verify the output file."""
-    cmd, outPath, name, checkFileAppend = args
-    try:
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        logger.info(result.stdout)
-    except subprocess.CalledProcessError as e:
-        logger.error("Error: %s", e)
-        logger.error("Stderr: %s", e.stderr)
-        raise
-
-    # Verify output file
-    outJpgFile = os.path.join(outPath, name + checkFileAppend)
-    outFileObj = Path(outJpgFile)
-    if not (outFileObj.is_file() and outFileObj.stat().st_size > 0):
-        raise ValueError(f"No image file generated: {outJpgFile}")
-
-    return f"Success: {cmd}"
-
 
 class PdbCsmImageWorkflow:
 
@@ -117,11 +97,31 @@ class PdbCsmImageWorkflow:
             else:
                 raise ValueError(f"Missing bcif file {bcifFilePath}")
         if numProcs == 1:
-            run_command(args[0])
+            self.run_command(args[0])
         else:
             # Execute commands in parallel with process-based execution
             with ProcessPoolExecutor(max_workers=int(numProcs)) as executor:
-                results = executor.map(run_command, args)
+                results = executor.map(self.run_command, args)
             # Print results
             for result in results:
                 logger.info(result)
+
+    # For pickling reasons this command needs to not accept self
+    def run_command(self, args):
+        """Run a command and verify the output file."""
+        cmd, outPath, name, checkFileAppend = args
+        try:
+            result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+            logger.info(result.stdout)
+        except subprocess.CalledProcessError as e:
+            logger.error("Error: %s", e)
+            logger.error("Stderr: %s", e.stderr)
+            raise
+
+        # Verify output file
+        outJpgFile = os.path.join(outPath, name + checkFileAppend)
+        outFileObj = Path(outJpgFile)
+        if not (outFileObj.is_file() and outFileObj.stat().st_size > 0):
+            raise ValueError(f"No image file generated: {outJpgFile}")
+
+        return f"Success: {cmd}"
