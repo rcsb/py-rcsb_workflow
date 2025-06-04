03b806d7b1eb0f33fa43be08d7a7a102bda22d01
Michael Trumbull <michael.trumbull@rcsb.org>
2025-06-04 10:11:33 -0400

    new hashing function and subdir scheme

---
+++ rcsb/workflow/utils/hashDirectories.py
@@ -0,0 +1,18 @@
+"""Hashing functions for file storage."""
+##
+# File: hashDirectories.py
+# Date: 04-Jun-2025  mjt
+#
+#  Shared hashing function  - for file generation
+#
+#  Updates:
+#  04-Jun-2025 mjt Created hashing function
+#
+##
+__docformat__ = "google en"
+__author__ = "Michael Trumbull"
+__email__ = "michael.trumbull@rcsb.org"
+__license__ = "Apache 2.0"
+
+def hash(id):
+    return id[1:3]
--- rcsb/workflow/wuw/PdbCsmImageWorkflow.py
+++ rcsb/workflow/wuw/PdbCsmImageWorkflow.py
@@ -20,6 +20,7 @@ import subprocess
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
 from rcsb.utils.io.MarshalUtil import MarshalUtil
+from rcsb.workflow.utils.hashDirectories import hash
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
 logger = logging.getLogger()
@@ -43,7 +44,6 @@ class PdbCsmImageWorkflow:
         csmBaseDir = kwargs.get("csmBaseDir")
         jpgsOutDir = kwargs.get("jpgsOutDir")
         contentTypeDir = kwargs.get("contentTypeDir")
-        useIdSubdir = kwargs.get("useIdSubdir")
         numProcs = kwargs.get("numProcs")
 
         logger.info("using id file %s", idListFile)
@@ -60,20 +60,18 @@ class PdbCsmImageWorkflow:
 
         # generate list of commands
         argsL = []
-        for line in idList:
+        for i, line in enumerate(idList):
             name = line.lower()
-            if useIdSubdir:
-                name = os.path.join(name[1:3], name)
 
-            bcifFileName = name + ".bcif.gz"
-            logger.info("Running %s %s %s", name, bcifFileName, contentTypeDir)
+            bcifFileName = os.path.join(hash(name), name) + ".bcif.gz"
+            logger.info("%s running %s %s %s", i, name, bcifFileName, contentTypeDir)
 
             if contentTypeDir == "pdb":
                 bcifFilePath = os.path.join(pdbBaseDir, bcifFileName)
             else:
                 bcifFilePath = os.path.join(csmBaseDir, bcifFileName)
 
-            outPath = os.path.join(jpgsOutDir, contentTypeDir)
+            outPath = os.path.join(jpgsOutDir, contentTypeDir, hash(name), name)
             Path(outPath).mkdir(parents=True, exist_ok=True)
 
             bcifFileObj = Path(bcifFilePath)
