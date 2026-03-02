56df19345e8d02616207290d1d5f9003e1006b47
Dennis Piehl <dennis.piehl@rcsb.org>
2026-03-02 14:35:02 -0500

    turn off debug tests

--- rcsb/workflow/tests/testLigandQualityReferenceGenerator.py
+++ rcsb/workflow/tests/testLigandQualityReferenceGenerator.py
@@ -25,7 +25,7 @@ TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
 
 
 class LigandQualityReferenceGeneratorTests(unittest.TestCase):
-    debugFlag = True
+    debugFlag = False
 
     def setUp(self):
         self.__isMac = platform.system() == "Darwin"
